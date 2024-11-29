04ef2d7e55405020b4bb8fa7ed06f5ae3c223c17
kageneko2525 <146046319+kageneko2525@users.noreply.github.com>
2024-11-29 12:41:57 +0900

    スレッドテーブル用DAO作成、機能の移し替え

---
+++ site/src/main/java/dao/ThreadDao.java
@@ -0,0 +1,142 @@
+package dao;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Timestamp;
+
+import model.ThreadDto;
+
+public class ThreadDao extends BaseDao {
+	/**
+	 * 最新のスレッドIDを検索
+	 * @param ThreadNumber int型
+	 * @return 最新のスレッドIDをint型で返す
+	 * @author x22u004
+	 */
+	public int findLatestThread() {
+	    int thread_id = 0; // デフォルト値を0に設定
+
+	    try {
+	        this.connect();
+
+	        String sql = "SELECT thread_id "
+	                   + "FROM thread "
+	                   + "WHERE thread_id = (SELECT MAX(thread_id) FROM thread);";
+
+	        PreparedStatement ps = con.prepareStatement(sql);
+
+	        ResultSet rs = ps.executeQuery();
+
+	        if (rs.next()) {
+	            // データが存在する場合はその値を取得
+	            thread_id = rs.getInt("thread_id");
+	        }
+
+	    } catch (Exception e) {
+	        e.printStackTrace();
+
+	    } finally {
+	        try {
+	            this.disConnect();
+	        } catch (SQLException e) {
+	            e.printStackTrace();
+	        }
+	    }
+
+	    return thread_id; // データがなかった場合はデフォルト値0を返す
+	}
+
+	
+	/**
+	 * 新しいスレッドをDB登録	
+	 * @author x22u004
+	 * @return 
+	 */
+	public void newCreateThread(int threadNo,String threadName,String threadUrl,Timestamp threadTime,int threadState) {		
+		System.out.println("DB Thread登録");
+		try {
+
+			this.connect();
+
+			String sql = "INSERT INTO thread(thread_id,thread_name,thread_url,thread_make,thread_state) VALUES(?,?,?,?,?);";
+			
+			PreparedStatement ps = con.prepareStatement(sql);
+			
+			ps.setInt(1, threadNo);
+			ps.setString(2, threadName);
+			ps.setString(3, threadUrl);
+			ps.setTimestamp(4, threadTime);
+			ps.setInt(5, threadState);
+			
+			ps.executeUpdate();
+			
+			ps.close();
+
+			
+		} catch (Exception e) {
+
+			e.printStackTrace();
+
+		} finally {
+
+			try {
+				
+				this.disConnect();
+				
+
+			} catch (SQLException e) {
+
+				e.printStackTrace();
+
+			}
+		}
+	}
+	
+	
+	
+	
+
+	
+	public void newCreateThread(ThreadDto threadDto) {		
+		System.out.println("DB Thread登録");
+		try {
+
+			this.connect();
+
+			String sql = "INSERT INTO thread(thread_id,thread_name,thread_url,thread_make,thread_state) VALUES(?,?,?,?,?);";
+			
+			PreparedStatement ps = con.prepareStatement(sql);
+			
+			ps.setInt(1, Integer.parseInt(threadDto.getId()));
+			ps.setString(2, threadDto.getTitle());
+			ps.setString(3, "");
+			ps.setTimestamp(4, threadDto.getCreateTime());
+			ps.setInt(5, 0);
+			
+			ps.executeUpdate();
+			
+			ps.close();
+
+			
+		} catch (Exception e) {
+
+			e.printStackTrace();
+
+		} finally {
+
+			try {
+				
+				this.disConnect();
+				
+
+			} catch (SQLException e) {
+
+				e.printStackTrace();
+
+			}
+		}
+	}
+	
+	
+}
