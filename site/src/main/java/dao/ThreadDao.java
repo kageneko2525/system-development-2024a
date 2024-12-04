01b159ba08a105b2c3aebfcb8c6c933f9e30b62a
kageneko2525 <146046319+kageneko2525@users.noreply.github.com>
2024-12-04 12:21:19 +0900

    スレッドDAO　コメントアウト追加、title検索や新しいもの検索追加

--- site/src/main/java/dao/ThreadDao.java
+++ site/src/main/java/dao/ThreadDao.java
@@ -10,12 +10,14 @@ import java.util.List;
 import model.ThreadDto;
 
 public class ThreadDao extends BaseDao {
-
-	/**
-	 * IDが新しいThredを10件返す？　未完につき放置しておいてください
-	 * @return ThredDto型のリストで返す
-	 */
-	public List<ThreadDto> LatestThread(int base, int count) {
+	
+/**
+ * IDの新しいスレッドを指定した場所から指定した数値分取得
+ * @param count 何件ほしいか
+ * @param offset 最初の何件飛ばすか
+ * @return Threadのリスト
+ */
+	public List<ThreadDto> getLatestThread(int count,int offset) {
 
 		//戻り値となるリスト宣言
 		List<ThreadDto> list = new ArrayList<ThreadDto>();
@@ -27,17 +29,20 @@ public class ThreadDao extends BaseDao {
 					+ "FROM thread "
 					+ "WHERE thread_state = 0 "
 					+ "ORDER BY thread_id ASC "
-					+ "LIMIT ?;";
+					+ "LIMIT ? OFFSET ?;";
 
 			PreparedStatement ps = con.prepareStatement(sql);
 
 			ps.setInt(1, count);
+			ps.setInt(2, offset);
 
 			ResultSet rs = ps.executeQuery();
 
 			while (rs.next()) {
 
-				ThreadDto thread = new ThreadDto();
+				ThreadDto thread = new ThreadDto(rs.getString("thread_id"),rs.getString("thread_name"),rs.getTimestamp("thread_make"));
+
+				list.add(thread);
 			}
 
 		} catch (Exception e) {
@@ -53,6 +58,63 @@ public class ThreadDao extends BaseDao {
 
 		return list; // データがなかった場合はデフォルト値1を返す
 	}
+
+	
+	
+	/**
+	 * タイトル検索 新しいもの
+	 * @param title タイトル
+	 * @param count 何件取得するか
+	 * @param offset 最初に何件を飛ばすか
+	 * @param threadState 生きてるスレッドか
+	 * @return リスト
+	 */
+public List<ThreadDto> getThreadTitleSortNew(String title,int count,int offset,int threadState) {
+	//戻り値となるリスト宣言
+	List<ThreadDto> list = new ArrayList<ThreadDto>();
+
+	try {
+		this.connect();
+
+		String sql = "SELECT * "
+				+ "FROM thread "
+				+ "WHERE thread_state = ? "
+				+ "AND thread_name LIKE '%?%'"
+				+ "ORDER BY thread_id ASC "
+				+ "LIMIT ? OFFSET ?;";
+		
+		PreparedStatement ps = con.prepareStatement(sql);
+
+		ps.setInt(1, threadState);
+		ps.setString(2, title);
+		ps.setInt(3, count);
+		ps.setInt(4, offset);
+
+		ResultSet rs = ps.executeQuery();
+
+		while (rs.next()) {
+
+			ThreadDto thread = new ThreadDto();
+			thread.setId(rs.getNString("id"));
+			thread.setTitle(rs.getString("title"));
+			thread.setCreateTime(rs.getTimestamp("CreateTime"));
+			list.add(thread);
+		}
+
+	} catch (Exception e) {
+		e.printStackTrace();
+
+	} finally {
+		try {
+			this.disConnect();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	return list; // データがなかった場合はデフォルト値1を返す
+}
+
 
 	/**
 	 * 最新のスレッドIDを検索
@@ -137,6 +199,12 @@ public class ThreadDao extends BaseDao {
 		}
 	}
 
+	
+	/**
+	 * 新しいthreadをDB登録
+	 * @param threadDto 登録したいスレッドのDTO
+	 * @author neko
+	 */
 	public void newCreateThread(ThreadDto threadDto) {
 		System.out.println("DB Thread登録");
 		try {
