3e673108c0fb01c0fd10ca27b62d85a5220fd2ee
x22u004 <x22u004@mitsuhashi.ac.jp>
2024-12-31 23:22:07 +0900

    ログインページと新規登録ページの追加

---
+++ site/src/main/webapp/js/login.js
@@ -0,0 +1,7 @@
+document.getElementById('loginForm').addEventListener('submit', function(event) {
+    event.preventDefault(); // フォームのデフォルトの送信動作を防止
+
+    const email = document.getElementById('email').value.trim();
+    const password = document.getElementById('password').value.trim();
+    
+});
---
+++ site/src/main/webapp/js/signup.js
@@ -0,0 +1,7 @@
+document.getElementById('signupForm').addEventListener('submit', function(event) {
+    event.preventDefault(); // フォーム送信を防止
+
+    const email = document.getElementById('email').value.trim();
+    const password = document.getElementById('password').value.trim();
+ 
+});
